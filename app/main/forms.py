37af3830909d1e79e8cef94f6e04a65f25e420cb
lorderonnie <rontheking45@gmail.com>
2019-12-01 22:49:03 +0300

    created the profile function

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,3 +1,10 @@
 from flask_wtf import  FlaskForm
 from wtforms import StringField,TextAreaField,SubmitField,SelectField
 from wtforms.validators import Required
+
+
+class UpdateProfile(FlaskForm):
+    bio = TextAreaField('BRief us on who you really are about you.',validators = [Required()])
+    submit = SubmitField('Submit')
+
+
--- app/models.py
+++ app/models.py
@@ -26,6 +26,8 @@ class User(UserMixin,db.Model):
     password_hash = db.Column(db.String(255))
     password_secure = db.Column(db.String(255))
     pass_secure = db.Column(db.String(255))
+    bio = db.Column(db.String(255))
+    profile_pic_path = db.Column(db.String())
     
     
     @property
